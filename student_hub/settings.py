37a9c1d1e5e2bc682e95d9e7565fc4fcc9916889
kalkidanyishak <94622211+kalkidanyishak@users.noreply.github.com>
2024-04-15 01:50:40 +0300

    Update settings.py

--- student_hub/settings.py
+++ student_hub/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['0.0.0.0', '1000']
+ALLOWED_HOSTS = []
 
 
 # Application definition
