8c8b09e6fb65e77172c6ee3fe07453ebd29d483a
kalkidanyishak <94622211+kalkidanyishak@users.noreply.github.com>
2024-04-15 01:44:18 +0300

    Update settings.py
    render.com

--- student_hub/settings.py
+++ student_hub/settings.py
@@ -33,9 +33,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = [0.0.0.0, 1000]
+ALLOWED_HOSTS = ['0.0.0.0', '1000']
 
 
 # Application definition
